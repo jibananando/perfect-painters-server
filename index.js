deff189aff71228c0328fc7bd4b9f374aa380d5d
jibananando <jibananando.pramanik@gmail.com>
2022-11-11 03:56:22 +0600

    insert data by mongodb and data collect to server site

--- index.js
+++ index.js
@@ -12,14 +12,28 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twm2yvw.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('perfectPainters').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => {
+    console.error(err);
+})
 
 app.get('/', (req, res) => {
     res.send('perfect painters running')
